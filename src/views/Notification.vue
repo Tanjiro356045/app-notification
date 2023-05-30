f204478e34b86b3f3142672e79818e6acb3e2f60
Tanjiro356045 <maxoulemat35@gmail.com>
2023-05-30 12:43:08 +0200

    TRIE PAR NOTIF

--- src/views/Notification.vue
+++ src/views/Notification.vue
@@ -1,60 +1,53 @@
 <template>
   <div id="app">
-
-    <ul>
-      <li v-for="notification in notifications" :key="notification.id" class="notification-item border border-dark">
-        <h2 class="text-center">{{ notification.title }}</h2>
-        <p class="text-muted textalignCenter">{{ notification.timestamp }}</p>
-
-        <div>
-          <p>{{ notification.message }}</p>
-        </div>
-      <div class="text-end">
-        <a class="btn btn-primary marginR10" href="{{ notification.url }}">{{ notification.url }}</a>
-        <a class="btn btn-danger" href="{{ notification.url }}"> X </a>
-      </div>
-      </li>
-    </ul>
+    <pre>{{ groupedNotifications }}</pre>
+    <ServiceNotifications
+        v-for="(serviceId, index) in serviceIds"
+        :key="index"
+        :serviceName="getServiceName(serviceId)"
+        :notifications="groupedNotifications[serviceId]"
+    />
   </div>
-
-
-
 </template>
 
 
+
 <style>
-
-ul {
-  list-style: none;
-}
-
-.notification-item {
-  margin-bottom: 30px;
+.service-header {
+  background-color: #f5f5f5;
   padding: 10px;
 }
-
-.textalignCenter {
-  text-align: center;
-}
-
-.marginR10 {
-  margin-right: 10px;
-}
-
-
-
-
-
 </style>
+
 <script>
+import ServiceNotifications from './ServiceNotifications.vue';
+
 export default {
+  components: {
+    ServiceNotifications,
+  },
   data() {
     return {
-      notifications: [] // tableau pour stocker les notifications récupérées
+      notifications: [], // tableau pour stocker les notifications récupérées
+      serviceIds: [], // tableau pour stocker les IDs de services
     };
   },
   mounted() {
     this.fetchNotifications(); // appel à la fonction fetchNotifications() lors du montage du composant
+  },
+  computed: {
+    groupedNotifications() {
+      // Regroupe les notifications par service
+      const grouped = {};
+      for (const notification of this.notifications) {
+        const serviceId = notification.servicesId;
+        if (!grouped[serviceId]) {
+          grouped[serviceId] = [];
+        }
+        grouped[serviceId].push(notification);
+      }
+      return grouped;
+    },
   },
   methods: {
     fetchNotifications() {
@@ -63,16 +56,23 @@ export default {
 
       // Effectuer l'appel API pour récupérer les notifications de l'utilisateur spécifié
       // Remplacez l'URL de l'API par votre propre URL, en incluant l'identifiant de l'utilisateur
-      fetch(`http://172.17.0.3/notification_pebble/public/api/notifications/user/${userId}`)
-          .then(response => response.json())
-          .then(data => {
+      fetch(`http://172.17.0.2/notification_pebble/public/api/notifications/user/${userId}`)
+          .then((response) => response.json())
+          .then((data) => {
             this.notifications = data; // Mettre à jour le tableau des notifications avec les données récupérées
+            this.serviceIds = Array.from(new Set(data.map((notification) => notification.servicesId))); // Récupérer les IDs de services uniques
           })
-          .catch(error => {
+          .catch((error) => {
             console.log(error);
           });
-    }
-  }
-};
+    },
 
+    getServiceName(serviceId) {
+      if (serviceId !== undefined) {
+        return serviceId.toString();
+      }
+      return '';
+    },
+  },
+};
 </script>
---
+++ src/views/ServiceNotifications.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="service-container">
+    <h2>{{ serviceName }}</h2>
+    <div class="notifications-container">
+      <ul>
+        <li v-for="notification in notifications" :key="notification.id" class="notification-item">
+          <h2 class="text-center">{{ notification.title }}</h2>
+          <p class="text-muted textalignCenter">{{ notification.timestamp }}</p>
+          <div>
+            <p>{{ notification.message }}</p>
+          </div>
+          <div class="text-end">
+            <a class="btn btn-primary marginR10" :href="notification.url">{{ notification.url }}</a>
+            <a class="btn btn-danger" :href="notification.url">X</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['serviceName', 'notifications'],
+};
+</script>
+
+<style>
+
+.service-container {
+  border: 3px solid red;  /* Let's make it more noticeable */
+  padding: 10px;
+  margin-bottom: 20px;
+}
+
+.notification-item {
+  border: 3px solid red;  /* Let's make it more noticeable */
+  padding: 10px;
+  margin-bottom: 20px;
+}
+</style>
